9bc9df64b39450550e806ab1536c0e774e4fdd05
matheusfornari@hotmail.com <Farfunheiro>
2020-10-05 22:01:15 -0300

    Inclusão FAG.teste.pas

--- Integrador.dpr
+++ Integrador.dpr
@@ -10,7 +10,8 @@ uses
   FAG.Usuario in 'FAG.Usuario.pas' {Form_Usuario},
   FAG.CadastroProduto in 'FAG.CadastroProduto.pas' {Form_CadastroProduto},
   FAG.CadastroUnMedida in 'FAG.CadastroUnMedida.pas' {Form_CadastroUnMedida},
-  FAG.Frame.Generico in 'FAG.Frame.Generico.pas' {Frame_Generico: TFrame};
+  FAG.Frame.Generico in 'FAG.Frame.Generico.pas' {Frame_Generico: TFrame},
+  FAG.teste in 'FAG.teste.pas' {Form_teste};
 
 {$R *.res}
 
@@ -19,5 +20,6 @@ begin
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TDataModuleConexao, DataModuleConexao);
   Application.CreateForm(TForm_Menu, Form_Menu);
+  Application.CreateForm(TForm_teste, Form_teste);
   Application.Run;
 end.
