17bc4d9492c1e28078b71e135388d495c6c43bbf
gabriel-kv@hotmail.com <gkverza>
2020-10-08 19:29:55 -0300

    Foi deletado as telas de usuario e teste

--- Integrador.dpr
+++ Integrador.dpr
@@ -7,11 +7,9 @@ uses
   FAG.Login in 'FAG.Login.pas' {Form_Login},
   FAG.DataModule.Conexao in 'FAG.DataModule.Conexao.pas' {DataModuleConexao: TDataModule},
   FAG.Menu in 'FAG.Menu.pas' {Form_Menu},
-  FAG.Usuario in 'FAG.Usuario.pas' {Form_Usuario},
   FAG.CadastroProduto in 'FAG.CadastroProduto.pas' {Form_CadastroProduto},
   FAG.CadastroUnMedida in 'FAG.CadastroUnMedida.pas' {Form_CadastroUnMedida},
   FAG.Frame.Generico in 'FAG.Frame.Generico.pas' {Frame_Generico: TFrame},
-  FAG.teste in 'FAG.teste.pas' {Form_teste},
   FAG.CadastroCategoria in 'FAG.CadastroCategoria.pas' {Form_CadastroCategoria};
 
 {$R *.res}
@@ -21,7 +19,6 @@ begin
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TDataModuleConexao, DataModuleConexao);
   Application.CreateForm(TForm_Menu, Form_Menu);
-  Application.CreateForm(TForm_teste, Form_teste);
   Application.CreateForm(TForm_CadastroCategoria, Form_CadastroCategoria);
   Application.Run;
 end.
